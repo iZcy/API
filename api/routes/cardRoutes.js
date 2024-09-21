80d4f81c16f9e6f8c3ba77aaaa8af006b739dc12
Khairazzz <oviekhairazayyan1204@mail.ugm.ac.id>
2024-09-21 17:14:32 +0700

    update delete

--- api/routes/cardRoutes.js
+++ api/routes/cardRoutes.js
@@ -6,6 +6,6 @@ router
   .get('/', cardControllers.cardsGet)        // Menggunakan cardsGet untuk mendapatkan semua kartu
   .post('/', cardControllers.cardsPost)      // Menggunakan cardsPost untuk membuat kartu baru
   .patch('/:id', cardControllers.cardsPatch)  // Menggunakan cardsPatch untuk memperbarui kartu
-  .delete('/', cardControllers.cardsDelete);  // Menggunakan cardsDelete untuk menghapus kartu
+  .delete('/:id', cardControllers.cardsDelete);  // Menggunakan cardsDelete untuk menghapus kartu
 
 module.exports = router;
