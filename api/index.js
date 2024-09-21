39f2850e639308d2c2dc85182614c047675e1cab
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2024-09-21 09:45:16 +0700

    feat: moreover setting, env, and nodemon

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 // server.js
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3500;
 
 // MongoDB connection
-const dbURI =
-  "mongodb+srv://kanbanpemweb22:XrrmKiflnQjoOVN3@kanbancluster.jndpg.mongodb.net/Task"; // Replace with your MongoDB URI
+const dbURI = process.env.DB_URI; // Replace with your MongoDB URI
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
@@ -14,7 +14,9 @@ mongoose
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Routes
 app.use("/", require("./routes/taskRoutes"));
 app.use("/task", require("./routes/taskRoutes"));
 app.use("/info", require("./routes/infoRoutes"));
@@ -22,4 +24,5 @@ app.use("/info", require("./routes/infoRoutes"));
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(`Mode: ${process.env.NODE_ENV}`);
 });
