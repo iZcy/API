700971f80e326e4f64df2461b1401b2b5a69147b
nibroosauro <nibroosauroremajiidharyanto@mail.ugm.ac.id>
2024-09-21 17:55:44 +0700

    standarisasi respon

--- api/controllers/boardControllers.js
+++ api/controllers/boardControllers.js
@@ -15,115 +15,142 @@ const deleteListsMiddleware = async (req, res, next) => {
 
     await Lists.deleteMany({ boardId: id });
 
-        next();
-    } catch (error) {
-        console.error("Error deleting lists related to the board:", error);
-        res.status(500).json({ message: "Error deleting lists related to the board" });
-    }
+    next();
+  } catch (error) {
+    console.error("Error deleting lists related to the board:", error);
+    res
+      .status(500)
+      .json({ message: "Error deleting lists related to the board" });
+  }
 };
 
 const boardGet = async (req, res) => {
-    try {
-        const data = await Board.find().populate('userId', 'username');
-        res.status(200).send(data);
-    } catch (error) {
-        console.error("Error retrieving boards:", error);
-        res.status(500).send("Error retrieving boards");
-    }
+  try {
+    const data = await Board.find().populate("userId", "username");
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error retrieving boards:", error);
+    res.status(500).json({ message: "Error retrieving boards" });
+  }
 };
 
 const boardPost = async (req, res) => {
   try {
-    const { title, userId, description, visibility } = req.body;
+    const { title, description, visibility } = req.body;
 
-        if (!title) return res.status(400).json({ message: "Title is required" });
-        if (!userId) return res.status(400).json({ message: "User ID is required" });
-        if (!visibility) return res.status(400).json({ message: "Visibility is required" });
-        if (!enums.visibilityEnum.includes(visibility)) return res.status(400).json({ message: "Invalid visibility value" });
+    const userId = req.user._id;
 
-        if (typeof title !== "string") return res.status(400).json({ message: "Invalid title: Wrong Type" });
-        if (!mongoose.Types.ObjectId.isValid(userId)) return res.status(400).json({ message: "Invalid userId: Must be a valid ObjectId" });
-        if (description && typeof description !== "string") return res.status(400).json({ message: "Invalid description: Wrong Type" });
+    if (!userId) return res.status(401).json({ message: "Unauthorized" });
 
-        const userExists = await User.findById(userId);
-        if (!userExists) return res.status(404).json({ message: "User ID not found" });
+    // Validation checks
 
-        const newBoard = new Board({
-            title,
-            userId,
-            description,
-            createdBy: userExists.username, 
-            visibility
-        });
+    if (!title) return res.status(400).json({ message: "Title is required" });
+    if (!visibility)
+      return res.status(400).json({ message: "Visibility is required" });
+    if (!enums.visibilityEnum.includes(visibility))
+      return res.status(400).json({ message: "Invalid visibility value" });
 
-        await newBoard.save();
-        res.status(201).json({ message: "Board created", data: newBoard });
-    } catch (error) {
-        console.error("Error saving board:", error);
-        if (error.name === 'ValidationError') {
-            return res.status(400).json({ message: `Validation Error: ${error.message}` });
-        }
-        res.status(500).send("Error saving board");
+    if (typeof title !== "string")
+      return res.status(400).json({ message: "Invalid title: Wrong Type" });
+    if (!mongoose.Types.ObjectId.isValid(userId))
+      return res
+        .status(400)
+        .json({ message: "Invalid userId: Must be a valid ObjectId" });
+    if (description && typeof description !== "string")
+      return res
+        .status(400)
+        .json({ message: "Invalid description: Wrong Type" });
+
+    const userExists = await User.findById(userId);
+    if (!userExists)
+      return res.status(404).json({ message: "User ID not found" });
+
+    const newBoard = new Board({
+      title,
+      userId,
+      description,
+      createdBy: userExists.username,
+      visibility,
+    });
+
+    await newBoard.save();
+    res.status(201).json({ message: "Board created", data: newBoard });
+  } catch (error) {
+    console.error("Error saving board:", error);
+    if (error.name === "ValidationError") {
+      return res
+        .status(400)
+        .json({ message: `Validation Error: ${error.message}` });
     }
+    res.status(500).json({ message: "Error saving board" });
+  }
 };
 
 const boardPatch = async (req, res) => {
-    try {
-        const { title, description, visibility } = req.body;
-        const { id } = req.params; 
+  try {
+    const { title, description, visibility } = req.body;
+    const { id } = req.params;
 
-        // Validation checks
-        if (!mongoose.Types.ObjectId.isValid(id)) return res.status(400).json({ message: "Invalid boardId: Must be a valid ObjectId" });
+    // Validation checks
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return res
+        .status(400)
+        .json({ message: "Invalid boardId: Must be a valid ObjectId" });
 
-        const board = await Board.findById(id);
-        if (!board) {
-            return res.status(404).send("Board not found");
-        }
-
-        // Check data types
-        if (title && typeof title !== "string") return res.status(400).json({ message: "Invalid title: Wrong Type" });
-        if (description && typeof description !== "string") return res.status(400).json({ message: "Invalid description: Wrong Type" });
-        if (visibility && !enums.visibilityEnum.includes(visibility)) return res.status(400).json({ message: "Invalid visibility value" });
-
-        // Update board fields
-        board.title = title || board.title;
-        board.description = description || board.description;
-        board.visibility = visibility || board.visibility;
-
-        await board.save();
-
-        res.status(200).json({ message: "Board updated", board });
-    } catch (error) {
-        console.error("Error updating board:", error);
-        res.status(500).send("Error updating board");
+    const board = await Board.findById(id);
+    if (!board) {
+      return res.status(404).json({ message: "Board not found" });
     }
+
+    // Check data types
+    if (title && typeof title !== "string")
+      return res.status(400).json({ message: "Invalid title: Wrong Type" });
+    if (description && typeof description !== "string")
+      return res
+        .status(400)
+        .json({ message: "Invalid description: Wrong Type" });
+    if (visibility && !enums.visibilityEnum.includes(visibility))
+      return res.status(400).json({ message: "Invalid visibility value" });
+
+    // Update board fields
+    board.title = title || board.title;
+    board.description = description || board.description;
+    board.visibility = visibility || board.visibility;
+
+    await board.save();
+
+    res.status(200).json({ message: "Board updated", board });
+  } catch (error) {
+    console.error("Error updating board:", error);
+    res.status(500).json({ message: "Error updating board" });
+  }
 };
 
 const boardDelete = async (req, res) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        // Validate Board ID
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-            return res.status(400).json({ message: "Invalid Board ID" });
-        }
-
-        const board = await Board.findByIdAndDelete(id);
-        if (!board) {
-            return res.status(404).json({ message: "Board not found" });
-        }
-
-        res.status(200).json({ message: "Board deleted", board });
-    } catch (error) {
-        console.error("Error deleting board:", error);
-        res.status(500).send("Error deleting board");
+    // Validate Board ID
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "Invalid Board ID" });
     }
+
+    const board = await Board.findByIdAndDelete(id);
+    if (!board) {
+      return res.status(404).json({ message: "Board not found" });
+    }
+
+    res.status(200).json({ message: "Board deleted", board });
+  } catch (error) {
+    console.error("Error deleting board:", error);
+    res.status(500).json({ message: "Error deleting board" });
+  }
 };
 
 module.exports = {
-    boardPost,
-    boardGet,
-    boardPatch,
-    boardDelete,
-    deleteListsMiddleware
+  boardPost,
+  boardGet,
+  boardPatch,
+  boardDelete,
+  deleteListsMiddleware,
 };
--- api/routes/boardRoutes.js
+++ api/routes/boardRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const boardController = require("../controllers/boardControllers");
+const accessControl = require("../middleware/accessControl");
+
+router.get("/", boardController.boardGet);
+router.use(accessControl.parseTokenDataMiddleware);
 
 router
-    .post("/", boardController.boardPost)
-    .get("/", boardController.boardGet)
-    .patch("/:id", boardController.boardPatch)
-    .delete("/:id", boardController.boardDelete);
+  .post("/", boardController.boardPost)
+  .patch("/:id", boardController.boardPatch)
+  .delete("/:id", boardController.boardDelete);
 
 module.exports = router;
