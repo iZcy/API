e639b57a36cf6dd9e14914ff316112dc93448d82
sulaimanfawwazak <sulaimanfawwazak@gmail.com>
2024-09-21 11:33:09 +0700

    Fix: fix the response message to use JSON format

--- api/controllers/commentsControllers.js
+++ api/controllers/commentsControllers.js
@@ -3,51 +3,60 @@ const Comments = require("../models/commentsModels");
 const commentsGet = async (req, res) => {
   try {
     const data = await Comments.find();
-    res.status(200).send(data);
+    // res.status(200).send(data);
+    res.status(200).json({ data });
   }
   catch (error) {
     console.log(error);
-    res.status(500).send("Error getting tasks");
+    // res.status(500).send("Error getting tasks");
+    res.status(500).json({ error: "Error getting tasks" });
   }
 };
 
 const commentsPost = async (req, res) => {
   try {
-    const { cardId, userId, content } = req.body;
+    const { cardId, userId, content, isEdited } = req.body;
 
     const newComments = new Comments({
       cardId,
       userId,
       content,
-      isEdited: false // Set isEdited to false when first created
+      // isEdited: false // Set isEdited to false when first created
+      isEdited// Set isEdited to false when first created
     });
     await newComments.save();
-    res.status(200).send(newComments + " saved");
+    // res.status(200).send(newComments + " saved");
+    res.status(201).json({ data: "Comment created", comment: newComments });
   }
   catch (error) {
     console.log(error);
-    res.status(500).send("Error saving comments");
+    res.status(500).json({ error: "Error saving comments" });
   }
 };
 
 const commentsPatch = async (req, res) => {
   try {
-    const { id, content } = req.body;
+    // const { id, content } = req.body;
+    const { id, cardId, userId, content, isEdited } = req.body;
     const data = await Comments.findById(id);
 
     if (!data) {
-      return res.status(404).send("Comments not found");
+      return res.status(404).json({ error: "Comments not found" });
     }
-
+    
+    data.cardId = cardId;
+    data.userId = userId;
     data.content = content;
     data.isEdited = true;
 
     await data.save();
-    res.status(200).send(data + " updated");
+    // res.status(200).send(data + " updated");
+    res.status(200).json({ data: "Comment updated", comment: data });
   }
   catch (error) {
     console.log(error);
-    res.status(500).send("Error updating comments");
+    // res.status(500).send("Error updating comments");
+    res.status(500).send({ error: "Error updating comments" });
   }
 };
 
@@ -56,11 +65,16 @@ const commentsDelete = async (req, res) => {
     const { id } = req.body;
     await Comments.findByIdAndDelete(id);
 
-    res.status(200).send("Task deleted");
+    if (!result) {
+      return res.status(404).json({ error: "Comment not found" });
+    }
+
+    // res.status(200).send("Task deleted");
+    res.status(200).json({ data: "Comment deleted" });
   }
   catch (error) {
     console.log(error);
-    res.status(500).send("Error deleting comments");
+    res.status(500).json({ error: "Error deleting comments" });
   }
 };
 
