1b2adc9c62a35bcf7d88737e251b1f833c451f44
Khairazzz <oviekhairazayyan1204@mail.ugm.ac.id>
2024-09-21 11:53:44 +0700

    card v3

--- api/routes/cardRoutes.js
+++ api/routes/cardRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const cardController = require('../controllers/cardController');
+const cardController = require('../controllers/cardControllers');
 
 router
   .get('/', cardController.getAllCards)
