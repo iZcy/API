a6babb205d89cd47b9e7ec61505b777c7f63c485
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2024-09-21 16:54:01 +0700

    feat: basic access middleware

--- api/controllers/authControllers.js
+++ api/controllers/authControllers.js
@@ -2,8 +2,9 @@ const User = require("../models/userModels");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const enums = require("../helper/enumerations");
+const accessControl = require("../middleware/accessControl");
 
-const cookieName = "kanbanapitoken";
+const cookieName = process.env.COOKIE_NAME;
 
 //domain check
 const cookiesOptionsGen = () => {
@@ -17,6 +18,14 @@ const cookiesOptionsGen = () => {
   };
 
   return settings;
+};
+
+const nonAdminConstraint = async (userData, accountToAction, callback) => {
+  const isNotAdmin = userData.role !== "admin";
+  const isTargetAdmin = accountToAction.role === "admin";
+  if (isNotAdmin || isTargetAdmin) {
+    return await callback();
+  }
 };
 
 const userRegister = async (req, res) => {
@@ -72,6 +81,22 @@ const userRegister = async (req, res) => {
       return res.status(400).json({ data: "Invalid role: Invalid Variant" });
     }
 
+    // Use middleware to check if user is allowed to register
+    // admin can only be registered by admin
+    // guest can regiester themselves (without credentials)
+    // member is a promoted guest, yet can't register themselves and can't promote others
+
+    if (role !== "guest") {
+      const response = await accessControl.allowedRole("admin")(
+        req,
+        res,
+        () => {}
+      );
+
+      // if the response is sent, return
+      if (response) return response;
+    }
+
     // Check if user exists
     const user = await User.findOne({ email });
     if (user) {
@@ -98,6 +123,11 @@ const userRegister = async (req, res) => {
 
 const userLogin = async (req, res) => {
   try {
+    // If cookies exist, remove them
+    if (req.cookies) {
+      res.clearCookie(cookieName, cookiesOptionsGen());
+    }
+
     // Body parsing
     const { email, password } = req.body;
 
@@ -269,31 +299,15 @@ const userDelete = async (req, res) => {
   try {
     // Body parsing
     const { email, password } = req.body;
+    const userData = req.user;
+
+    // if userData is not found, return 403
+    if (!userData) {
+      return res.status(403).json({ data: "Forbidden to delete!" });
+    }
 
     // Check Body Existence
     if (!email) return res.status(400).json({ data: "Email is required" });
-
-    if (!password)
-      return res.status(400).json({ data: "Password is required" });
-
-    // Check Body Validity
-    const emailTooShort = email.length < 6;
-    const emailWrongType = typeof email !== "string";
-    if (emailWrongType) {
-      return res.status(400).json({ data: "Invalid email: Wrong Type" });
-    }
-    if (emailTooShort) {
-      return res.status(400).json({ data: "Invalid email: Too Short" });
-    }
-
-    const passwordTooShort = password.length < 6;
-    const passwordWrongType = typeof password !== "string";
-    if (passwordWrongType) {
-      return res.status(400).json({ data: "Invalid password: Wrong Type" });
-    }
-    if (passwordTooShort) {
-      return res.status(400).json({ data: "Invalid password: Too Short" });
-    }
 
     // Check if user exists
     const user = await User.findOne({ email });
@@ -301,12 +315,38 @@ const userDelete = async (req, res) => {
       return res.status(400).json({ data: "User does not exist" });
     }
 
-    const validPassword = await bcrypt.compare(password, user.passwordHash);
-    if (!validPassword) {
-      return res.status(400).json({ data: "Invalid password" });
-    }
+    // If the to delete user is an admin, only himself can delete by providing the password
+    const resp = await nonAdminConstraint(userData, user, async () => {
+      // Check if current user is the account owner
+      if (userData.email !== email) {
+        return res.status(400).json({ data: "This account is not yours" });
+      }
+
+      if (!password)
+        return res.status(400).json({ data: "Password is required" });
+
+      const passwordTooShort = password.length < 6;
+      const passwordWrongType = typeof password !== "string";
+      if (passwordWrongType) {
+        return res.status(400).json({ data: "Invalid password: Wrong Type" });
+      }
+      if (passwordTooShort) {
+        return res.status(400).json({ data: "Invalid password: Too Short" });
+      }
+
+      const validPassword = await bcrypt.compare(password, user.passwordHash);
+      if (!validPassword) {
+        return res.status(400).json({ data: "Invalid password" });
+      }
+    });
+    if (resp) return resp;
 
     await User.deleteOne({ email });
+    // Logout if the user is deleting himself
+    if (userData.email === email) {
+      res.clearCookie(cookieName, cookiesOptionsGen());
+    }
+
     res.status(200).json({ data: "User deleted" });
   } catch (error) {
     console.log(error);
--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authControllers");
+const accessControl = require("../middleware/accessControl");
 
 router.post("/register", authController.userRegister);
 router.post("/login", authController.userLogin);
 router.get("/logout", authController.userLogout);
 router.get("/role", authController.userRole);
 router.patch("/change-password", authController.userChangePassword);
-router.delete("/delete", authController.userDelete);
+router.delete(
+  "/delete",
+  accessControl.parseTokenDataMiddleware,
+  authController.userDelete
+);
 router.patch("/update", authController.userUpdate);
 
 module.exports = router;
