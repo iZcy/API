bd7fb7596cf2b33312a3f80cb4ddd7b06e59a5fd
abeputra <emirabe.ea@gmail.com>
2024-09-21 12:37:19 +0700

    "Update 1"

--- api/controllers/listsControllers.js
+++ api/controllers/listsControllers.js
@@ -2,11 +2,11 @@ const List = require("../models/listsModels");
 
 const listsGet = async (req, res) => {
   try {
-    const data = await Task.find();
+    const data = await Lists.find();
     res.status(200).send(data);
   } catch (error) {
     console.log(error);
-    res.status(500).send("Error getting tasks");
+    res.status(500).send("Error getting Listss");
   }
 };
 
@@ -14,7 +14,7 @@ const listsPost = async (req, res) => {
   try {
     const { listId, title, boardId, position, createdBy } = req.body;
 
-    const newTask = new Task({
+    const newLists = new Lists({
       listId,
       title,
       boardId,
@@ -22,21 +22,21 @@ const listsPost = async (req, res) => {
       createdBy
     });
 
-    await newTask.save();
-    res.status(200).send(newTask + " saved");
+    await newLists.save();
+    res.status(200).send(newLists + " saved");
   } catch (error) {
     console.log(error);
-    res.status(500).send("Error saving task");
+    res.status(500).send("Error saving Lists");
   }
 };
 
 const listsPatch = async (req, res) => {
   try {
     const { id, listId, title, boardId, position, createdBy } = req.body;
-    const data = await Task.findById(id);
+    const data = await Lists.findById(id);
 
     if (!data) {
-      return res.status(404).send("Task not found");
+      return res.status(404).send("Lists not found");
     }
 
     data.listId = listId || data.listId;
@@ -49,19 +49,19 @@ const listsPatch = async (req, res) => {
     res.status(200).send(data + " updated");
   } catch (error) {
     console.log(error);
-    res.status(500).send("Error updating task");
+    res.status(500).send("Error updating Lists");
   }
 };
 
 const listsDelete = async (req, res) => {
   try {
     const { id } = req.body;
-    await Task.findByIdAndDelete(id);
+    await Lists.findByIdAndDelete(id);
 
-    res.status(200).send("Task deleted");
+    res.status(200).send("Lists deleted");
   } catch (error) {
     console.log(error);
-    res.status(500).send("Error deleting task");
+    res.status(500).send("Error deleting Lists");
   }
 };
 
