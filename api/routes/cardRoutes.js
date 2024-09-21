1e890461663c8940e2694632e11823eee28465f1
Khairazzz <oviekhairazayyan1204@mail.ugm.ac.id>
2024-09-21 11:18:42 +0700

    card v2

--- api/routes/cardRoutes.js
+++ api/routes/cardRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const cardController = require('../controllers/cardController');
 
-// Routes for cards
-router.post('/', cardController.createCard); // Create card
-router.get('/', cardController.getAllCards); // Get all cards
-router.get('/:id', cardController.getCardById); // Get card by ID
-router.put('/:id', cardController.updateCard); // Update card
-router.delete('/:id', cardController.deleteCard); // Delete card
+router
+  .get('/', cardController.getAllCards)
+  .post('/', cardController.createCard)
+  .get('/:id', cardController.getCardById)
+  .patch('/:id', cardController.updateCard)
+  .delete('/:id', cardController.deleteCard);
+
+
 
 module.exports = router;
