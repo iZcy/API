c32e39be99be3917f4635abe567daf762fa19ad4
sulaimanfawwazak <sulaimanfawwazak@gmail.com>
2024-09-21 14:24:34 +0700

    Fix: fix the data validation in the commentsContollers.js

--- api/controllers/commentsControllers.js
+++ api/controllers/commentsControllers.js
@@ -1,8 +1,15 @@
 const Comments = require("../models/commentsModels");
+const User = require("../models/userModels");
+const Card = require("../models/cardModels");
 
 const commentsGet = async (req, res) => {
   try {
     const data = await Comments.find();
+
+    if (!data) {
+      return res.status(404).json({ error: "Comment not found"});
+    }
+
     // res.status(200).send(data);
     res.status(200).json({ data });
   }
@@ -15,8 +22,32 @@ const commentsGet = async (req, res) => {
 
 const commentsPost = async (req, res) => {
   try {
+    // Body parsing
     // const { cardId, userId, content, isEdited } = req.body;
     const { cardId, userId, content } = req.body;
+    // const { userId, content } = req.body;
+
+    // Check body existance
+    if (!cardId) {
+      return res.status(400).json({ data: "cardId is required!"});
+    }
+    if (!userId) {
+      return res.status(400).json({ data: "userId is required!"});
+    }
+    if (!content) {
+      return res.status(400).json({ data: "content is required!"});
+    }
+
+    // Check if cardId exists
+    const card_check = await Card.findOne({ _id: cardId });
+    if (!card_check) {
+      return res.status(400).json({ data: "cardId doesn't exist!"});
+    }
+    // Check if userId exists
+    const user_check = await User.findOne({ _id: userdId });
+    if (!user_check) {
+      return res.status(400).json({ data: "userId doesn't exist!"});
+    }
 
     const newComments = new Comments({
       cardId,
@@ -37,11 +68,43 @@ const commentsPost = async (req, res) => {
 
 const commentsPatch = async (req, res) => {
   try {
+    // Body parsing
     // const { id, content } = req.body;
     // const { id, cardId, userId, content, isEdited } = req.body;
     const { id, cardId, userId, content } = req.body;
     const data = await Comments.findById(id);
 
+    // Check body existance
+    if (!id) {
+      res.status(400).json({ data: "id is required!" });
+    }
+    if (!cardId) {
+      res.status(400).json({ data: "cardId is required!" });
+    }
+    if (!userId) {
+      res.status(400).json({ data: "userId is required!" });
+    }
+    if (!content) {
+      res.status(400).json({ data: "content is required!" });
+    }
+
+    // Check if id exists
+    const id_check = await Comments.findOne({ id });
+    if (!id_check) {
+      return res.status(400).json({ data: "id doesn't exist!"});
+    }
+    // Check if cardId exists
+    const card_check = await Card.findOne({ _id: cardId });
+    if (!card_check) {
+      return res.status(400).json({ data: "cardId doesn't exist!"});
+    }
+    // Check if userId exists
+    const user_check = await User.findOne({ _id: userdId });
+    if (!user_check) {
+      return res.status(400).json({ data: "userId doesn't exist!"});
+    }
+
+    // Check if data is returned
     if (!data) {
       return res.status(404).json({ error: "Comments not found" });
     }
@@ -64,9 +127,22 @@ const commentsPatch = async (req, res) => {
 
 const commentsDelete = async (req, res) => {
   try {
+    // Body parsing
     const { id } = req.body;
     const result = await Comments.findByIdAndDelete(id);
 
+    // Check bddy existance
+    if (!id) {
+      return res.status(404).json({ data: "id is required!" });
+    }
+
+    // Check if id exists
+    const id_check = await Comments.findOne({ id });
+    if (!id_check) {
+      return res.status(400).json({ data: "id doesn't exist!"});
+    }
+
+    // Check if result is returned
     if (!result) {
       return res.status(404).json({ error: "Comment not found" });
     }
