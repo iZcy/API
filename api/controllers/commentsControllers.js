9e3de0b8b7d7febe0862cee7020b2c50150020f2
sulaimanfawwazak <sulaimanfawwazak@gmail.com>
2024-09-24 13:30:06 +0700

    Fix: refactor commentsControllers.js

--- api/controllers/commentsControllers.js
+++ api/controllers/commentsControllers.js
@@ -7,16 +7,12 @@ const commentsGet = async (req, res) => {
   try {
     const data = await Comments.find();
 
-    if (!data) {
-      return res.status(404).json({ error: "Comment not found"});
-    }
+    if (!data) return res.status(404).json({ error: "Comment not found"});
 
-    // res.status(200).send(data);
     res.status(200).json({ data });
   }
   catch (error) {
     console.log(error);
-    // res.status(500).send("Error getting tasks");
     res.status(500).json({ error: "Error getting tasks" });
   }
 };
@@ -29,15 +25,9 @@ const commentsPost = async (req, res) => {
     // const { userId, content } = req.body;
 
     // Check body existance
-    if (!cardId) {
-      return res.status(400).json({ error: "cardId is required!"});
-    }
-    if (!userId) {
-      return res.status(400).json({ error: "userId is required!"});
-    }
-    if (!content) {
-      return res.status(400).json({ error: "content is required!"});
-    }
+    if (!cardId) return res.status(400).json({ error: "cardId is required!"});
+    if (!userId) return res.status(400).json({ error: "userId is required!"});
+    if (!content) return res.status(400).json({ error: "content is required!"});
 
     // Check the data types
     if (!mongoose.Types.ObjectId.isValid(cardId)) return res.status(400).json({ data: "cardId type must be ObjectId"});
@@ -46,14 +36,11 @@ const commentsPost = async (req, res) => {
 
     // Check if cardId exists
     const card_check = await Card.findOne({ _id: cardId });
-    if (!card_check) {
-      return res.status(400).json({ error: "cardId doesn't exist!"});
-    }
+    if (!card_check) return res.status(400).json({ error: "cardId doesn't exist!"});
+
     // Check if userId exists
     const user_check = await User.findOne({ _id: userId });
-    if (!user_check) {
-      return res.status(400).json({ error: "userId doesn't exist!"});
-    }
+    if (!user_check) return res.status(400).json({ error: "userId doesn't exist!"});
 
     const newComments = new Comments({
       cardId,
@@ -62,8 +49,8 @@ const commentsPost = async (req, res) => {
       // isEdited: false // Set isEdited to false when first created
       // isEdited
     });
+    
     await newComments.save();
-    // res.status(200).send(newComments + " saved");
     res.status(201).json({ message: "Comment created", comment: newComments });
   }
   catch (error) {
@@ -81,18 +68,10 @@ const commentsPatch = async (req, res) => {
     const data = await Comments.findById(id);
 
     // Check body existance
-    if (!id) {
-      res.status(400).json({ error: "id is required!" });
-    }
-    if (!cardId) {
-      res.status(400).json({ error: "cardId is required!" });
-    }
-    if (!userId) {
-      res.status(400).json({ error: "userId is required!" });
-    }
-    if (!content) {
-      res.status(400).json({ error: "content is required!" });
-    }
+    if (!id) res.status(400).json({ error: "id is required!" });
+    if (!cardId) res.status(400).json({ error: "cardId is required!" });
+    if (!userId) res.status(400).json({ error: "userId is required!" });
+    if (!content) res.status(400).json({ error: "content is required!" });
 
     // Check the data types
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(400).json({ data: "id type must be ObjectId"});
@@ -102,24 +81,18 @@ const commentsPatch = async (req, res) => {
 
     // Check if id exists
     const id_check = await Comments.findOne({ id });
-    if (!id_check) {
-      return res.status(400).json({ error: "id doesn't exist!"});
-    }
+    if (!id_check) return res.status(400).json({ error: "id doesn't exist!"});
+
     // Check if cardId exists
     const card_check = await Card.findOne({ _id: cardId });
-    if (!card_check) {
-      return res.status(400).json({ error: "cardId doesn't exist!"});
-    }
+    if (!card_check) return res.status(400).json({ error: "cardId doesn't exist!"});
+
     // Check if userId exists
     const user_check = await User.findOne({ _id: userdId });
-    if (!user_check) {
-      return res.status(400).json({ error: "userId doesn't exist!"});
-    }
+    if (!user_check) return res.status(400).json({ error: "userId doesn't exist!"});
 
     // Check if data is returned
-    if (!data) {
-      return res.status(404).json({ error: "Comments not found" });
-    }
+    if (!data) return res.status(404).json({ error: "Comments not found" });
     
     data.cardId = cardId;
     data.userId = userId;
@@ -127,12 +100,10 @@ const commentsPatch = async (req, res) => {
     data.isEdited = true;
 
     await data.save();
-    // res.status(200).send(data + " updated");
     res.status(200).json({ message: "Comment updated", comment: data });
   }
   catch (error) {
     console.log(error);
-    // res.status(500).send("Error updating comments");
     res.status(500).json({ error: "Error updating comments" });
   }
 };
@@ -144,24 +115,16 @@ const commentsDelete = async (req, res) => {
     const result = await Comments.findByIdAndDelete(id);
 
     // Check bddy existance
-    if (!id) {
-      return res.status(404).json({ error: "id is required!" });
-    }
-
+    if (!id) return res.status(404).json({ error: "id is required!" });
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(400).json({ data: "id type must be ObjectId"});
 
     // Check if id exists
     const id_check = await Comments.findOne({ id });
-    if (!id_check) {
-      return res.status(400).json({ error: "id doesn't exist!"});
-    }
+    if (!id_check) return res.status(400).json({ error: "id doesn't exist!"});
 
     // Check if result is returned
-    if (!result) {
-      return res.status(404).json({ error: "Comment not found" });
-    }
+    if (!result) return res.status(404).json({ error: "Comment not found" });
 
-    // res.status(200).send("Task deleted");
     res.status(200).json({ message: "Comment deleted" });
   }
   catch (error) {
