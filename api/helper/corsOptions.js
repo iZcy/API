2381b3f267b669c5916daea9ebd21f2d55513105
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2024-09-25 10:35:36 +0700

    fix: cors public

--- api/helper/corsOptions.js
+++ api/helper/corsOptions.js
@@ -1,20 +1,16 @@
-const allowedOrigins =
-  process.env.NODE_ENV === "development"
-    ? ["http://localhost:3000", undefined] // undefined allows Postman and anonymous access
-    : [process.env.FE_URL];
-
 const corsOptions = {
   origin: (origin, callback) => {
-    // Allow requests with no origin (like mobile apps, Postman, etc.) or from allowed origins
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    // Temporarily allow all origins for testing
+    if (
+      !origin ||
+      allowedOrigins.includes(origin) ||
+      process.env.NODE_ENV === "production"
+    ) {
       callback(null, true);
     } else {
-      console.error(`CORS error: Origin ${origin} not allowed`);
-      callback(new Error(`${origin} Not allowed by CORS`));
+      callback(new Error("CORS not allowed"));
     }
   },
-  credentials: true, // Ensure credentials are allowed
-  optionsSuccessStatus: 200 // Response status for successful preflight requests
+  credentials: true,
+  optionsSuccessStatus: 200
 };
-
-module.exports = corsOptions;
