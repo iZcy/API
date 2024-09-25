15cf5c05c71657dc70db270261cb10d169d729de
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2024-09-25 10:45:34 +0700

    fix: cors (3)

--- api/controllers/authControllers.js
+++ api/controllers/authControllers.js
@@ -13,7 +13,7 @@ const cookiesOptionsGen = () => {
     sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
     domain:
       process.env.NODE_ENV === "development" ? "localhost" : process.env.FE_URL,
-    secure: process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV !== "development",
     maxAge: 24 * 60 * 60 * 1000
   };
 
